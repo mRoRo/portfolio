108659794ba8672d4bb5d953e1ab834a25e9fdf1
María Rodríguez <maria.rodriguez1@gmail.com>
2026-03-17 20:28:37 +0100

    fix: update siteUrl to mariarodriguez.dev

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   defaultLang: 'en',
   secondaryLang: 'es',
-  siteUrl: 'https://mariarodriguezrojo.dev',
+  siteUrl: 'https://mariarodriguez.dev',
 } as const;
 
 export type Lang = typeof siteConfig.defaultLang | typeof siteConfig.secondaryLang;
